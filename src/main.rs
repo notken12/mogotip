a3f17ffaf4264f505e93ed4be74d7bc63bca0ccb
Ken Zhou <kendotzhou@gmail.com>
2022-05-18 22:12:55 -0400

    add option to dump all mogo tips to stdout

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,25 @@
+use clap::Parser;
 use rand::{self, Rng};
+
+/// Program to show tips about our lord Mogo
+#[derive(Parser)]
+#[clap(author, version, about, long_about=None)]
+struct Args {
+    /// Print all Mogo tips
+    #[clap(short, long)]
+    all: bool,
+}
 
 const TIPS: &str = include_str!("mogo.txt");
 
 fn main() {
-    let lines: Vec<&str> = TIPS.split('\n').collect();
-    let mut rng = rand::thread_rng();
-    let index = rng.gen_range(0..lines.len());
-    println!("{}", lines[index]);
+    let args = Args::parse();
+    if args.all {
+        print!("{}", TIPS);
+    } else {
+        let lines: Vec<&str> = TIPS.split('\n').collect();
+        let mut rng = rand::thread_rng();
+        let index = rng.gen_range(0..lines.len());
+        println!("{}", lines[index]);
+    }
 }
